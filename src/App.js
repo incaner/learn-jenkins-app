79345b01024d1f483ff12cdbb9d5294a7b515b47
NAP-Programing <120453847+incaner@users.noreply.github.com>
2024-11-23 22:41:39 +0000

    changes to webside

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins on Crack :)
         </a>
       </header>
       <p>
-          Application version: 1
+          Application version: 1.1
       </p>
     </div>
   );
